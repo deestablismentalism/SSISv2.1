d6fb882d832b2c6ed47318ca09f7b37849cd7f1c
Lovely <lovelycruz.1317@gmail.com>
2025-09-17 03:12:54 +0800

    fixed styling for about page team section and counters

--- FrontEnd/About_Page.php
+++ FrontEnd/About_Page.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="./assets/css/landing-header.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 
     <title>SSIS-About Page</title>
@@ -20,9 +24,9 @@
         <!-- Hero Section -->
         <section class="hero">
             <div class="hero-slideshow">
-                <div class="hero-slide active"
+                <!-- <div class="hero-slide active"
                     style="background-image: linear-gradient(rgba(104, 165, 184, 0.32), rgba(44, 130, 156, 0.6)), url('./assets/imgs/test.png')">
-                </div>
+                </div> -->
                 <div class="hero-slide"
                     style="background-image: linear-gradient(rgba(104, 165, 184, 0.32), rgba(44, 130, 156, 0.6)), url('./assets/imgs/teacher.jpg')">
                 </div>
--- FrontEnd/Learn_More_Page.php
+++ FrontEnd/Learn_More_Page.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/learn-more.css">
     <link rel="stylesheet" href="./assets/css/landing-header.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
     
     <title>SSIS-Learn More Page</title>
 </head>
@@ -21,9 +24,9 @@
         <!-- Hero Section -->
         <section class="hero">
             <div class="hero-slideshow">
-                <div class="hero-slide active"
+                <!-- <div class="hero-slide active"
                     style="background-image: linear-gradient(rgba(104, 165, 184, 0.32), rgba(44, 130, 156, 0.6)), url('./assets/imgs/test.png')">
-                </div>
+                </div> -->
                 <div class="hero-slide"
                     style="background-image: linear-gradient(rgba(104, 165, 184, 0.32), rgba(44, 130, 156, 0.6)), url('./assets/imgs/teacher.jpg')">
                 </div>
@@ -38,7 +41,7 @@
                 </p>
                 <br>
                 <h1 class="hero-title">
-                    <span class="word">Timog Dos,</span> <span class="word">Dunong ay Lubos</span>
+                    <span class="word">Learn More</span>
                 </h1>
             </div>
 
@@ -123,7 +126,7 @@
             <div class="learn-more-container">
                 <div class="learn-more-content">
                     <div class="image-text-container">
-                        <img src="./assets/imgs/students.jp" alt="Image 1" class="image">
+                        <!-- <img src="./assets/imgs/student.jpg" alt="Image 1" class="image"> -->
                         <div class="text-container">
                             <h3>Our Beloved School</h3>
                             <p>Lucena South II Elementary School is dedicated to improving teaching and learning through 
@@ -136,7 +139,7 @@
                 </div>
                 <div class="learn-more-content">
                     <div class="image-text-container">
-                        <img src="./assets/imgs/teacher.jp" alt="Image 1" class="image">
+                        <!-- <img src="./assets/imgs/teacher.jpg" alt="Image 1" class="image"> -->
                         <div class="text-container">
                             <h3>Our Teachers</h3>
                             <p>Our dedicated teachers are committed to providing quality education and support to all students.
@@ -149,7 +152,7 @@
                 </div>
                 <div class="learn-more-content">
                     <div class="image-text-container">
-                        <img src="./assets/imgs/grad.jp" alt="Image 1" class="image">
+                        <!-- <img src="./assets/imgs/grad.jpg" alt="Image 1" class="image"> -->
                         <div class="text-container">
                             <h3>History</h3>
                             <p>Lucena South II Elementary School was established in 1963 and has since played a vital 
@@ -160,7 +163,7 @@
                 </div>
                 <div class="learn-more-content">
                     <div class="image-text-container">
-                        <img src="./assets/imgs/planting.jp" alt="Image 1" class="image">
+                        <!-- <img src="./assets/imgs/planting.jp" alt="Image 1" class="image"> -->
                         <div class="text-container">
                              <h3>Community</h3>
                             <p>The school thrives through the strong support of its local community. Parents, stakeholders, 
