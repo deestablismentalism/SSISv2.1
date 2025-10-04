b71e326c96ba4e89009db51114e028cccd1c322f
Lovely <157420039+lovelycruz.1317@users.noreply.github.com>
2025-10-04 09:40:15 +0800

    Added student grading module(Not functional yet)

--- FrontEnd/pages/teacher/teacher_subjects_handled.php
+++ FrontEnd/pages/teacher/teacher_subjects_handled.php
@@ -5,6 +5,7 @@ if(session_status() === PHP_SESSION_NONE) {
 ob_start();
 require_once __DIR__ . '/../../../BackEnd/teacher/view/teacherSubjectsHandledView.php';
 $view = new teacherSubjectsHandledView();
+$pageTitle = 'Subjects Handled';
 $pageCss = '<link rel="stylesheet" href="../../assets/css/teacher/teacher-subjects-handled.css">';
 $pageJs = '<script src="../../assets/js/teacher/teacher-subjects-handled.js" defer></script>';
 
