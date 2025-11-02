92d7fee5d81957964190ec91a66a216107800df6
Frostydayz <157420039+Frostydayz@users.noreply.github.com>
2025-11-02 13:03:02 +0800

    Added terms and conditions for registration

--- BackEnd/api/postRegistrationForm.php
+++ BackEnd/api/postRegistrationForm.php
@@ -8,19 +8,26 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     echo json_encode(['success'=> false, 'message'=> 'Invalid request method']);
     exit();
 }
-    $controller = new Registration();
-        
-    // PERSONAL INFORMATION
-    $First_Name = $_POST['Guardian-First-Name'] ?? null;
-    $Last_Name = $_POST['Guardian-Last-Name'] ?? null;
-    $Middle_Name = $_POST['Guardian-Middle-Name'] ?? null;
-    $Contact_Number = $_POST['Contact-Number'] ?? null;
-    $User_Type = 3;
-    
-    
-    $response = $controller->registerAndInsert($First_Name, $Last_Name, $Middle_Name, $Contact_Number, $User_Type);
-    http_response_code($response['httpcode']);
-    echo json_encode($response);
-    exit(); 
 
+// Validate terms acceptance
+$termsAcceptance = $_POST['terms-acceptance'] ?? null;
+if ($termsAcceptance !== 'on') {
+    echo json_encode(['success'=> false, 'message'=> 'You must accept the Terms & Conditions']);
+    http_response_code(400);
+    exit();
+}
+
+$controller = new Registration();
+        
+// PERSONAL INFORMATION
+$First_Name = $_POST['Guardian-First-Name'] ?? null;
+$Last_Name = $_POST['Guardian-Last-Name'] ?? null;
+$Middle_Name = $_POST['Guardian-Middle-Name'] ?? null;
+$Contact_Number = $_POST['Contact-Number'] ?? null;
+$User_Type = 3;
+
+$response = $controller->registerAndInsert($First_Name, $Last_Name, $Middle_Name, $Contact_Number, $User_Type);
+http_response_code($response['httpcode']);
+echo json_encode($response);
+exit(); 
 ?>
