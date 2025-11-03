9663e2889f4f933ebafb24dc533b38939b67c356
deestablismentalism <davidiyad6@gmail.com>
2025-11-04 00:06:32 +0800

    Switched to dynamic table creation

--- BackEnd/admin/view/adminAllEnrolleesView.php
+++ BackEnd/admin/view/adminAllEnrolleesView.php
@@ -1,17 +1,17 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/./models/adminEnrolleesModel.php';
-
+require_once __DIR__ . '/../controller/adminEnrolleesController.php';
+require_once __DIR__ . '/../../core/tableDataTemplate.php';
+require_once __DIR__ . '/../../core/safeHTML.php';
 class adminAllEnrolleesView {
-    protected $conn;
-    protected $getAllEnrollees;
-    protected $getAllEnrolleesCount;
+    protected $tableTemplate;
+    protected $controller;
     public const ENROLLED = 1;
     public const DENIED = 2;
     public const PENDING = 3;
     public const TO_FOLLOW = 4;
 
-    public function stringEquivalent(int $value): string {
+    private function stringEquivalent(int $value): string {
        switch($value) {
             case self::ENROLLED:
                 return "enrolled";
@@ -25,52 +25,65 @@ class adminAllEnrolleesView {
                 return "unknown";
         }
     }
-
     public function __construct() {
-        $db = new Connect();
-        $this->conn = $db->getConnection();
-        $enrollee = new adminEnrolleesModel();
-        $this->getAllEnrollees = $enrollee->getAllEnrollees();
-        $this->getAllEnrolleesCount = $enrollee->countAllEnrollees();
+        $this->controller = new adminEnrolleesController();
+        $this->tableTemplate = new TableCreator();
     }
-
     public function displayCount() {
         try {
-            $count = $this->getAllEnrolleesCount;
-            echo htmlspecialchars($count);
-        }
-        catch(PDOException $e) {
-            die("Query Failed: " . $e->getMessage());
-        }
-    }
-
-    public function displayAllEnrollees() {
-        try {
-            $data = $this->getAllEnrollees;
-            foreach($data as $rows) {   
-                $parentMiddleInitial = substr($rows['Middle_Name'], 0, 1) . ".";
-                $hasLrn = $rows['Learner_Reference_Number'] == 0 ? 'No LRN' :  $rows['Learner_Reference_Number'] ;
-                $studentMiddleInitial = substr($rows['Student_Middle_Name'], 0, 1) . ".";
-                $status = $this->stringEquivalent((int)$rows['Enrollment_Status']);
-                
-                echo '<tr class="enrollee-row"> 
-                        <td>' . $hasLrn . '</td>
-                        <td>' .htmlspecialchars($rows['Student_Last_Name']) . ', ' 
-                        .htmlspecialchars($rows['Student_First_Name']) . ' ' 
-                        .htmlspecialchars($studentMiddleInitial) . '</td>
-                        <td>' . htmlspecialchars($rows['E_Grade_Level']) . '</td>
-                        <td>' . htmlspecialchars($status) . '</td>
-                        <td>' . htmlspecialchars($rows['Last_Name']) . ', ' . htmlspecialchars($rows['First_Name']) . ' ' 
-                               .htmlspecialchars($parentMiddleInitial) . '</td> 
-                        <td>' 
-                        . htmlspecialchars($rows['Contact_Number']) . 
-                        '</td>
-                        <td> <button class="view-button" data-id="' . $rows['Enrollee_Id'] . '"> View info</button><td>
-                        </tr>';
+            $data = $this->controller->viewTransactionsCount();
+            if(!$data['success']) {
+                echo '<div class="error-message">' .htmlspecialchars($data['message']).'</div>';
+                return;
+            }
+            else {
+                echo $data['data'];
             }
         }
-        catch(PDOException $e) {
-            die("Query Failed: " . $e->getMessage());
+        catch(Exception $e) {
+            echo '<div class="error-message"> There was an unexpected problem </div>';
         }
+    }
+    public function displayAllTransactions() {
+        try {
+            $data = $this->controller->viewAllEnrollmentTransactions();
+            if(!$data['success'] || empty($data['data'])) {
+                echo '<div class="error-message">' .htmlspecialchars($data['message']).'</div>';
+            }else {
+                echo '<table>';
+                echo $this->tableTemplate->returnHorizontalTitles([
+                'Student Name', 'Transaction Code', 'Handled By', 'Handled At','Enrollment Status Given', 
+                'Transaction Status', 'View Info'
+                ],'enrollees-title');
+                echo '<tbody>';
+                foreach($data['data'] as $rows) {   
+                //Student name
+                $firstName = !empty($rows['Student_First_Name']) ? $rows['Student_First_Name'] : 'No First name found';
+                $lastName = !empty($rows['Student_Last_Name']) ? $rows['Student_Last_Name'] : 'No Last name found';
+                $middleName = !empty($rows['Student_Middle_Name']) ? $rows['Student_Middle_Name'] : '';
+                $studentFull = (!empty($firstName) && !empty($lastName)) 
+                ? $lastName . ', '.$firstName . ' ' . $middleName : 'Name is empty';
+                $transactionCode = $rows['Transaction_Code'];
+                //Staff name
+                $staffFirstName = !empty($rows['Staff_First_Name']) ? $rows['Staff_First_Name'] : 'No First name found';
+                $staffLastName = !empty($rows['Staff_Last_Name']) ? $rows['Staff_Last_Name'] : 'No Last name found';
+                $staffMiddleName = !empty($rows['Staff_Middle_Name']) ? $rows['Staff_Middle_Name'] : '';
+                $staffFullName = (!empty($staffFirstName) && !empty($staffLastName)) 
+                ? $staffLastName . ', '.$staffFirstName . ' ' . $staffMiddleName : 'Name is empty';
+                $handledAt = $rows['Created_At'];
+                $transactionStatus = ($rows['Is_Approved'] === 0) ? 'Unprocessed' : 'Finalized';
+                $givenStatus = strtoupper($this->stringEquivalent((int)$rows['Enrollment_Status']));
+                $button = new safeHTML('<button class="view-enrollee">View</button>');
+                
+                echo $this->tableTemplate->returnHorizontalRows([
+                    $studentFull, $transactionCode, $staffFullName, $handledAt, $givenStatus, $transactionStatus, $button
+                ],'enrollee-row');
+            }
+            echo '</tbody></table>';
+        }
+    }
+    catch(Throwable $t) {
+        echo '<div class="error-message">There was a syntax problem. Please wait for it to be fixed</div>';
+    }
     }
 } 
