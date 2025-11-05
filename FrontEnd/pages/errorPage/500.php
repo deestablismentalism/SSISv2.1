dcc2d8f020db7ce5dba022a5238e6f15fc1fe3fc
deestablismentalism <davidiyad6@gmail.com>
2025-11-05 13:43:26 +0800

    Handled error 500 page to be able to contain query string of proper pages(Need to add directory in models)

--- FrontEnd/pages/errorPage/500.php
+++ FrontEnd/pages/errorPage/500.php
@@ -1,7 +1,9 @@
 <body style="background-color: antiquewhite;">
+    <?php $uri = $_GET['from'] ?? '../../Login.php'?>
     <div style="display:flex;justify-content: center">
         <p>
             HTTP Response 500: There was a problem with the server! Please check your Internet connection or contact the school to confirm
-        </p>
+        </p><br>
+        <a href="../../pages/<?php echo $uri?>"> Try to Reconnect?</a>
     </div>
 </body>
