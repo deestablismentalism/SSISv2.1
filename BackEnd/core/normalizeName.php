1f79f0cf9d9b0fab327bb05cd079c27b005817ce
deestablismentalism <davidiyad6@gmail.com>
2025-11-13 22:36:47 +0800

    Added dots in normalize names

--- BackEnd/core/normalizeName.php
+++ BackEnd/core/normalizeName.php
@@ -6,7 +6,7 @@ class normalizeName {
     public function normalize():string {
         $lCaseName = strtolower($this->name);
 
-        $lCaseName = preg_replace_callback("/(?:^|[\s'-])(\p{L})/u",function($matches){
+        $lCaseName = preg_replace_callback("/(?:^|[\s'\-\.])(\p{L})/u",function($matches){
             return strtoupper($matches[0]);
         },$lCaseName);
         return $lCaseName;
@@ -14,7 +14,7 @@ class normalizeName {
     public function isValid(): bool {
         if (is_null($this->name)) return false;
         // Reject if anything other than letters, spaces, hyphens, or apostrophes
-        return (bool)preg_match("/^[\p{L}\s'-]+$/u", $this->name);
+        return (bool)preg_match("/^[\p{L}\s'\-\.]+$/u", $this->name);
     }
     public function validatedNormalize(): string {
         if (!$this->isValid()) {
